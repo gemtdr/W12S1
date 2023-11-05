fd74b33eb00f3b9f7d5d7b0d7b8e0c6fffd5f960
gemtdr <123492499+gemtdr@users.noreply.github.com>
2023-11-05 13:45:05 -0600

    Update W12S1.py

--- W12S1.py
+++ W12S1.py
@@ -1,50 +1,46 @@
 class Time:
 
-    def __init__(self, seconds = 0):
+    def __init__(self): #make a default parameter called seconds and set it to 0
         
         self.hours = 0
         self.minutes = 0
-        self.seconds = seconds
 
-    def addTimes(self,rhs):
+    def addTimes(self,rhs): #Update this function to include seconds
         
         addedTime = Time()
         addedTime.hours = self.hours + rhs.hours
         addedTime.minutes = self.minutes + rhs.minutes
-        addedTime.seconds = self.seconds + rhs.seconds
+     
 
         return addedTime
 
-    def subtractTimes(self,rhs):
+    def subtractTimes(self,rhs): #Use the logic from addTimes to complete this function
 
-        subtractedTime = Time()
-        subtractedTime.hours = self.hours - rhs.hours
-        subtractedTime.minutes = self.minutes - rhs.minutes
-        subtractedTime.seconds = self.seconds - rhs.seconds
+        pass
+    
 
-    def clearTime(self):
+    def clearTime(self): #Set hours, minutes and seconds to 0
 
-        self.hours = 0
-        self.minutes = 0
-        self.seconds = 0
+        pass
+    
 
 
-    def printTime(self):
+    def printTime(self): #update to include seconds
 
-        print(f'Hours: {self.hours}, Minutes: {self.minutes}, Seconds: {self.seconds}')
+        print(f'Hours: {self.hours}, Minutes: {self.minutes}')
 
 
 
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #Play with the instances below to test each of your functions
 
     time1 = Time()
     time1.hours = 7
     time1.minutes = 15
 
-    time2 = Time(3)
+    time2 = Time()
     time2.hours = 5
     time2.minutes = 2
 
